f3156a658be177eaee3f7f7398c7de9eea90acf1
harryh987 <h_wowaccount1@web.de>
2018-12-02 18:56:28 +0100

    schon ein paar lösungen für aufgabe 1

---
+++ aufgabe1.fs
@@ -0,0 +1,33 @@
+//legt liste mit strings an
+let fruit = ["apple"; "banana"; "orange"; "plum"; "cherry"]
+//erstelle eine neue liste mit zwei weiteren string und der fruit-liste
+let fruit2 = "strawberry"::"lemon"::fruit
+printfn "%A" fruit2
+//neue liste mit einer range von 1 bis 9
+let list1 = [0..9]
+printfn "%A" list1
+//eine neue liste mit strings
+let fruit3 = ["ananas"; "apricot"]
+//konkatiniere zwei listen
+let fruit4 = fruit2@fruit3
+printfn "%A" fruit4
+//konkatiniere zwei listen, nur andersherum
+let fruit5 = fruit3@fruit2
+printfn "%A" fruit5
+
+//zeige nur das head-element von fruit5
+printfn "fruit5 head:%A" fruit5.Head
+//zeige nur das tail-element/die tail-elemente von fruit5 (also die ganze liste nur ohne head)
+printfn "fruit5 tail:%A" fruit5.Tail
+
+//benutze die .sum-funktion von listen um die summe einer range von zahlen zu berechnen
+let sum1 = List.sum [1 .. 10]
+printfn "Summe von 1 bis 10: %i" sum1
+
+//benutze die -average-funktion von listen um den durschnitt der elemente einer liste zu berechnen
+let sum2 = List.average [1.0 .. 10.0]
+printfn "Durchschnitt von 1 bis 10: %f" sum2
+
+
+//let rec berechneSumme values accum =
+//  if x < 1 then 1
